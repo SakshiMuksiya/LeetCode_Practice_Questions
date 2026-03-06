353e809aec4e3725be69cbb93dd62339c7ec0505
Sakshi Muksiya <sakshimuksiya@gmail.com>
2026-03-06 22:06:08 +0530

    Add files via upload

---
+++ Leetcode_practice_questions/arrays_and_strings/minimum_size_subarray_sum.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
+
+#solution
+def minSubArrayLen(target: int, nums) -> int:
+        min_len=float("inf")
+        left=0
+        cur_sum=0
+        for right in range(len(nums)):
+            cur_sum += nums[right]
+            while cur_sum >=target:
+                if right-left+1 < min_len:
+                    min_len= right-left+1
+                cur_sum -= nums[left]
+                left+=1
+        return min_len if min_len != float("inf") else 0

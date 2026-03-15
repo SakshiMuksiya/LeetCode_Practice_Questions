b229442f6496a3ae2345959a2303ed84a1c0d50c
Sakshi Muksiya <sakshimuksiya@gmail.com>
2026-03-15 22:53:33 +0530

    solved problem to rotate matrix in-place
    Implement in-place rotation of a matrix by 90 degrees.

---
+++ Leetcode_practice_questions/arrays_and_strings/rotate_matrix.py
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/rotate-image/submissions/1949327778/?envType=study-plan-v2&envId=top-interview-150
+
+#solution
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        col = len(matrix[0])
+
+        for i in range(row):
+            for j in range(i + 1, col):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for k in range(row):
+            matrix[k].reverse()

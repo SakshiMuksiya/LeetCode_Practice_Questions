9f58fe56013e7f8e19defc889f2e8337bf4a6d8c
Sakshi Muksiya <sakshimuksiya@gmail.com>
2026-03-27 23:09:39 +0530

    Create valid_parantheses.py

---
+++ Leetcode_practice_questions/arrays_and_strings/valid_parantheses.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/valid-parentheses/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")":"(", "}":"{", "]":"["}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        
+        return not stack

09686640729304243a1c4bc6e89045e1d91df5fb
Sakshi Muksiya <sakshimuksiya@gmail.com>
2026-04-01 18:57:36 +0530

    Add function to compare two linked lists
    Implement function to compare two linked lists for equality.

---
+++ Leetcode_practice_questions/linked_list/compare_linked_lists.py
@@ -0,0 +1,17 @@
+#https://www.hackerrank.com/contests/testlinkedlist/challenges/compare-two-linked-lists
+
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def compare_lists(llist1, llist2):
+    while llist1 and llist2:
+        if llist1.data != llist2.data:
+            return 0
+        llist1 = llist1.next
+        llist2 = llist2.next
+     
+    if llist1 is None and llist2 is None:
+        return 1
+    return 0
